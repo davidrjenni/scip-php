0ac8ea638f6f76787d01d743863acde32b4466ca
David Jenni <me@davidrjenni.io>
2023-04-09 21:17:34 +0200

    src: add indexer

---
+++ src/DocIndexer.php
@@ -0,0 +1,188 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ScipPhp;
+
+use PhpParser\Node;
+use PhpParser\Node\Const_;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\NullsafeMethodCall;
+use PhpParser\Node\Expr\NullsafePropertyFetch;
+use PhpParser\Node\Expr\PropertyFetch;
+use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Expr\StaticPropertyFetch;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\FunctionLike;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
+use PhpParser\Node\Param;
+use PhpParser\Node\Stmt\ClassConst;
+use PhpParser\Node\Stmt\ClassLike;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\EnumCase;
+use PhpParser\Node\Stmt\Namespace_;
+use PhpParser\Node\Stmt\Property;
+use PhpParser\Node\Stmt\PropertyProperty;
+use Scip\Occurrence;
+use Scip\SymbolInformation;
+use Scip\SymbolRole;
+use Scip\SyntaxKind;
+use ScipPhp\Composer\Composer;
+use ScipPhp\Parser\PosResolver;
+use ScipPhp\Types\Types;
+
+use function is_string;
+use function str_starts_with;
+
+final class DocIndexer
+{
+    /** @var array<non-empty-string, SymbolInformation> */
+    public array $symbols;
+
+    /** @var array<non-empty-string, SymbolInformation> */
+    public array $extSymbols;
+
+    /** @var array<int, Occurrence> */
+    public array $occurrences;
+
+    public function __construct(
+        private readonly Composer $composer,
+        private readonly SymbolNamer $namer,
+        private readonly Types $types,
+    ) {
+        $this->symbols = [];
+        $this->extSymbols = [];
+        $this->occurrences = [];
+    }
+
+    public function index(PosResolver $pos, Node $n): void
+    {
+        // ------- Definitions -------
+
+        if ($n instanceof ClassConst) {
+            foreach ($n->consts as $c) {
+                $this->def($pos, $c, $c->name, SyntaxKind::IdentifierConstant);
+            }
+            return;
+        }
+        if ($n instanceof ClassLike && $n->name !== null) {
+            $this->def($pos, $n, $n->name, SyntaxKind::IdentifierType);
+            return;
+        }
+        if ($n instanceof ClassMethod) {
+            $this->def($pos, $n, $n->name);
+            return;
+        }
+        if ($n instanceof Param && $n->var instanceof Variable && is_string($n->var->name)) {
+            // Constructor property promotion.
+            if ($n->flags !== 0) {
+                $p = new PropertyProperty($n->var->name, $n->default, $n->getAttributes());
+                $this->def($pos, $p, $n->var, SyntaxKind::IdentifierParameter);
+            }
+            return;
+        }
+        if ($n instanceof Property) {
+            foreach ($n->props as $p) {
+                $this->def($pos, $p, $p->name);
+            }
+            return;
+        }
+
+        // ------- Usages -------
+
+        if ($n instanceof ClassConstFetch && $n->name instanceof Identifier && $n->name->toString() !== '') {
+            $symbol = $this->types->constDef($n->class, $n->name->toString());
+            if ($symbol !== null) {
+                $this->ref($pos, $symbol, $n->name, SyntaxKind::IdentifierConstant);
+            }
+            return;
+        }
+        if (
+            ($n instanceof MethodCall || $n instanceof NullsafeMethodCall || $n instanceof StaticCall)
+            && $n->name instanceof Identifier
+            && $n->name->toString() !== ''
+        ) {
+            $symbol = $n instanceof StaticCall
+                ? $this->types->methDef($n->class, $n->name->toString())
+                : $this->types->methDef($n->var, $n->name->toString());
+            if ($symbol !== null) {
+                $this->ref($pos, $symbol, $n->name);
+            }
+            return;
+        }
+        if ($n instanceof Name) {
+            if ($n->getAttribute('parent') instanceof Namespace_) {
+                return;
+            }
+            $symbol = $this->types->nameDef($n);
+            if ($symbol !== null) {
+                $this->ref($pos, $symbol, $n);
+            }
+            return;
+        }
+        if (
+            ($n instanceof PropertyFetch || $n instanceof NullsafePropertyFetch || $n instanceof StaticPropertyFetch)
+            && $n->name instanceof Identifier
+            && $n->name->toString() !== ''
+        ) {
+            $symbol = $n instanceof StaticPropertyFetch
+                ? $this->types->propDef($n->class, $n->name->toString())
+                : $this->types->propDef($n->var, $n->name->toString());
+            if ($symbol !== null) {
+                $this->ref($pos, $symbol, $n->name);
+            }
+            return;
+        }
+    }
+
+    /** @param  Const_|ClassLike|EnumCase|FunctionLike|Name|Param|PropertyProperty|non-empty-string  $n */
+    private function def(
+        PosResolver $pos,
+        Const_|ClassLike|EnumCase|FunctionLike|Name|Param|PropertyProperty|string $n,
+        Node $posNode,
+        int $kind = SyntaxKind::Identifier,
+    ): void {
+        $symbol = !is_string($n) ? $this->namer->name($n) : $n;
+        if ($symbol === null) {
+            return;
+        }
+        $this->symbols[$symbol] = new SymbolInformation([
+            'symbol'        => $symbol,
+            'documentation' => [$symbol], // TODO(drj): build hover content
+        ]);
+        $this->occurrences[] = new Occurrence([
+            'range'        => $pos->pos($posNode),
+            'symbol'       => $symbol,
+            'symbol_roles' => SymbolRole::Definition,
+            'syntax_kind'  => $kind,
+        ]);
+    }
+
+    /** @param  non-empty-string  $symbol */
+    private function ref(
+        PosResolver $pos,
+        string $symbol,
+        Node $posNode,
+        int $kind = SyntaxKind::Identifier,
+        int $role = SymbolRole::UnspecifiedSymbolRole,
+    ): void {
+        if (!str_starts_with($symbol, 'local ')) {
+            $ident = $this->namer->extractIdent($symbol);
+            if ($this->composer->isDependency($ident)) {
+                $this->extSymbols[$symbol] = new SymbolInformation([
+                    'symbol'        => $symbol,
+                    'documentation' => [$symbol], // TODO(drj): build hover content
+                ]);
+            }
+        }
+
+        $this->occurrences[] = new Occurrence([
+            'range'        => $pos->pos($posNode),
+            'symbol'       => $symbol,
+            'symbol_roles' => $role,
+            'syntax_kind'  => $kind,
+        ]);
+    }
+}
---
+++ src/Indexer.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ScipPhp;
+
+use Closure;
+use Scip\Document;
+use Scip\Index;
+use Scip\Language;
+use Scip\Metadata;
+use ScipPhp\Composer\Composer;
+use ScipPhp\Parser\Parser;
+use ScipPhp\Types\Types;
+
+use function array_values;
+use function str_replace;
+
+final class Indexer
+{
+    private readonly Parser $parser;
+
+    private readonly Composer $composer;
+
+    private readonly SymbolNamer $namer;
+
+    private readonly Types $types;
+
+    /** @param  non-empty-string  $projectRoot */
+    public function __construct(
+        private readonly string $projectRoot,
+        private readonly Metadata $metadata,
+    ) {
+        $this->parser = new Parser();
+        $this->composer = new Composer($this->projectRoot);
+        $this->namer = new SymbolNamer($this->composer);
+        $this->types = new Types($this->composer, $this->namer);
+    }
+
+    public function index(): Index
+    {
+        $projectFiles = $this->composer->projectFiles();
+        $this->types->collect(...$projectFiles);
+
+        $documents = [];
+        $extSymbols = [];
+        foreach ($projectFiles as $filename) {
+            $indexer = new DocIndexer($this->composer, $this->namer, $this->types);
+            $this->parser->traverse($filename, $indexer, Closure::fromCallable([$indexer, 'index']));
+            $documents[] = new Document([
+                'language'      => Language::PHP,
+                'relative_path' => str_replace($this->projectRoot . '/', '', $filename),
+                'occurrences'   => $indexer->occurrences,
+                'symbols'       => array_values($indexer->symbols),
+            ]);
+            foreach ($indexer->extSymbols as $symbol => $info) {
+                $extSymbols[$symbol] = $info;
+            }
+        }
+
+        return new Index([
+            'documents'        => $documents,
+            'metadata'         => $this->metadata,
+            'external_symbols' => $extSymbols,
+        ]);
+    }
+}
