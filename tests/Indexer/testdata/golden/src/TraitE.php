07625cc69be9b08c900ad815119a3cdf34d07150
David Jenni <me@davidrjenni.io>
2023-10-30 17:08:49 +0100

    tests/Indexer/testdata/golden: fix golden files

--- tests/Indexer/testdata/golden/src/TraitE.php
+++ tests/Indexer/testdata/golden/src/TraitE.php
@@ -48,7 +48,7 @@
       {
           if (true) {
               return 23 - count([0]);
-//                        ^^^^^ reference scip-php composer php 8.2.11 count().
+//                        ^^^^^ reference scip-php composer php 8.2.12 count().
           }
           if (false) {
               return 42;
