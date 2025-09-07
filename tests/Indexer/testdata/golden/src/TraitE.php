7861df00e06dd657277c97dffdceea43c5503e1f
David Jenni <me@davidrjenni.io>
2025-09-07 15:48:54 +0200

    .github/workflows/ci.yml: update PHP and Go versions

--- tests/Indexer/testdata/golden/src/TraitE.php
+++ tests/Indexer/testdata/golden/src/TraitE.php
@@ -67,7 +67,7 @@
       {
           if (true) {
               return 23 - count([0]);
-//                        ^^^^^ reference scip-php composer php 8.3.23 count().
+//                        ^^^^^ reference scip-php composer php 8.4.12 count().
           }
           if (false) {
               return 42;
