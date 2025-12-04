c1b1fc2d255c62fad340b79faf0d6e47d6a32909
David Jenni <me@davidrjenni.io>
2025-12-04 21:30:55 +0100

    tests/Indexer: fix golden files

--- tests/Indexer/testdata/golden/src/ClassF.php
+++ tests/Indexer/testdata/golden/src/ClassF.php
@@ -5,7 +5,7 @@
   namespace TestData;
   
   use function strlen;
-//             ^^^^^^ reference scip-php composer php 8.4.13 strlen().
+//             ^^^^^^ reference scip-php composer php 8.4.14 strlen().
   
   /** @method ClassA m1(int $p1, $p2, bool $p3) */
 //                   ^^ definition scip-php composer davidrjenni/scip-php-test 2879a47ba00225b1d0cf31ebe8b9fc7f6cd28be5 TestData/ClassF#m1().
@@ -50,7 +50,7 @@
       {
           return $this->f1 + 42 + strlen('ABC');
 //                      ^^ reference scip-php composer davidrjenni/scip-php-test 2879a47ba00225b1d0cf31ebe8b9fc7f6cd28be5 TestData/ClassF#$f1.
-//                                ^^^^^^ reference scip-php composer php 8.4.13 strlen().
+//                                ^^^^^^ reference scip-php composer php 8.4.14 strlen().
       }
   
       public static function f2(): ClassA
--- tests/Indexer/testdata/golden/src/TraitE.php
+++ tests/Indexer/testdata/golden/src/TraitE.php
@@ -67,7 +67,7 @@
       {
           if (true) {
               return 23 - count([0]);
-//                        ^^^^^ reference scip-php composer php 8.4.13 count().
+//                        ^^^^^ reference scip-php composer php 8.4.14 count().
           }
           if (false) {
               return 42;
